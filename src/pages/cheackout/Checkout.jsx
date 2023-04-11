18cda46c1f8feeb43200a3a2035e9f0620120092
lanya yousif <lanyayusf541@gmail.com>
2023-04-11 04:20:50 +0300

    add to checkpoint page

--- src/pages/cheackout/Checkout.jsx
+++ src/pages/cheackout/Checkout.jsx
@@ -5,42 +5,40 @@ import { NavLink } from "react-router-dom";
 import Button from "../../component/button/Button";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faChevronLeft } from "@fortawesome/free-solid-svg-icons";
-import cartImg from '../../assets/img/cartImg.jpg'
+import cartImg from "../../assets/img/cartImg.jpg";
 
 function Checkout() {
-
-    const productONCartData=[
-        {
-            id:1,
-            name:"Scent Free 24-Hour Cream 50ml",
-            img:cartImg,
-            price:"$14"
-        },
-        {
-            id:1,
-            name:" Cream 50ml",
-            img:cartImg,
-            price:"$34"
-        },
-        {
-            id:1,
-            name:" Cream 50ml",
-            img:cartImg,
-            price:"$34"
-        },
-    ]
-
+  const productONCartData = [
+    {
+      id: 1,
+      name: "Scent Free 24-Hour Cream 50ml",
+      img: cartImg,
+      price: "$14",
+    },
+    {
+      id: 1,
+      name: " Cream 50ml",
+      img: cartImg,
+      price: "$34",
+    },
+    {
+      id: 1,
+      name: " Cream 50ml",
+      img: cartImg,
+      price: "$34",
+    },
+  ];
 
   return (
     <>
       <main className="checkout relative  min-h-[100vh]">
-        <div className="w-full h-full flex justify-center  flex-row min-h-[100vh]">
-          <div className="contactPart basis-7/12 px-10 pt-10 pb-14">
+        <div className="w-full h-full flex justify-center  flex-row min-h-[100vh] md:flex-col sm:flex-col xsm:flex-col">
+          <div className="contactPart lg:basis-7/12 xl:basis-7/12 2xl:basis-7/12 px-10 xsm:px-4 pt-10 pb-14">
             <Logo />
             <form action="#" className="mt-8 mx-auto max-w-[660px]">
               <div className="Emailpart h-fit ">
-                <div className="relative flex flex-row justify-between items-center  w-[90%] max-w-[600px]">
-                  <h3 className="h5 xsm:h6 ">Contact Information</h3>
+                <div className="relative flex flex-row justify-between items-center xsm:flex-col-reverse  w-[90%] xsm:w-full max-w-[600px]">
+                  <h3 className="h5 xsm:h6  ">Contact Information</h3>
                   <p className="text-base xsm:text-sm  ">
                     Already have an account? <NavLink to="/">login</NavLink>
                   </p>
@@ -49,7 +47,7 @@ function Checkout() {
                   <input
                     type="email"
                     placeholder="Email"
-                    className="checkInput block w-[90%] max-w-[600px] bg-transparent h-14 rounded px-4"
+                    className="checkInput block w-[90%]  xsm:w-[100%]  max-w-[600px] bg-transparent h-14 rounded px-4"
                     name="email"
                   />
                 </div>
@@ -60,10 +58,10 @@ function Checkout() {
                   <input
                     type="text"
                     placeholder="country/region"
-                    className="checkInput block w-[90%] max-w-[600px] bg-transparent h-14 rounded px-4 my-4"
+                    className="checkInput block w-[90%] xsm:w-[100%]  max-w-[600px] bg-transparent h-14 rounded px-4 my-4"
                     name="country/region"
                   />
-                  <div className="fullname  w-[90%] max-w-[600px] flex justify-between">
+                  <div className="fullname  w-[90%]  xsm:w-[100%]  max-w-[600px] flex justify-between">
                     <input
                       type="text"
                       placeholder="First name"
@@ -80,30 +78,30 @@ function Checkout() {
                   <input
                     type="text"
                     placeholder="address"
-                    className="checkInput block w-[90%] max-w-[600px] bg-transparent h-14 rounded px-4 my-2"
+                    className="checkInput block w-[90%] xsm:w-[100%]  max-w-[600px] bg-transparent h-14 rounded px-4 my-2"
                     name="address"
                   />
                   <input
                     type="text"
                     placeholder="apartment ,suit, etc "
-                    className="checkInput block w-[90%] max-w-[600px] bg-transparent h-14 rounded px-4 my-6"
+                    className="checkInput block w-[90%] xsm:w-[100%]  max-w-[600px] bg-transparent h-14 rounded px-4 my-6"
                     name="apartment ,suit, etc "
                   />
                   <input
                     type="text"
                     placeholder="city"
-                    className="checkInput block w-[90%] max-w-[600px] bg-transparent h-14 rounded px-4 my-6"
+                    className="checkInput block w-[90%] xsm:w-[100%] max-w-[600px] bg-transparent h-14 rounded px-4 my-6"
                     name="city"
                   />
                   <input
                     type="text"
                     placeholder="phone"
-                    className="checkInput block w-[90%] max-w-[600px] bg-transparent h-14 rounded px-4 my-6"
+                    className="checkInput block w-[90%]  xsm:w-[100%]  max-w-[600px] bg-transparent h-14 rounded px-4 my-6"
                     name="phone"
                   />
                 </div>
               </div>
-              <div className="buttonAndReturnPart  w-[90%] max-w-[590px] flex justify-between items-center">
+              <div className="buttonAndReturnPart  w-[90%] xsm:w-full xsm:flex-col-reverse max-w-[590px] flex justify-between items-center">
                 <div className="returnCart">
                   <NavLink to="/">
                     {" "}
@@ -119,23 +117,31 @@ function Checkout() {
               </div>
             </form>
           </div>
-          <div className="totalProduct_part basis-5/12 bg-bg-bejiKal pt-20 ">
-                {
-                    productONCartData.map(data=>{
-                        return <ProductONCart {...data} key={data.id} />
-                    })   
-                }
+          <div className="totalProduct_part lg:basis-5/12 xl:basis-5/12 2xl:basis-5/12  bg-bg-bejiKal pt-20 pb-16 ">
+            <div className="mx-auto max-w-[660px]">
+              {productONCartData.map((data) => {
+                return <ProductONCart {...data} key={data.id} />;
+              })}
 
-                <div className="totalAndsub mx-8 xsm:mx-4 pt-32">
-                    <div className="subtotal py-5 mb-10">
-                        <p className="text-base font-medium capitalize inline-block float-left">subtotal</p>
-                        <p className="subtotalPrice inline-block float-right font-semibold">$122</p>
-                    </div>
-                    <div className="subtotal py-5">
-                        <p className="text-base font-medium capitalize inline-block float-left">total</p>
-                        <p className="subtotalPrice inline-block float-right font-semibold">$122</p>
-                    </div>
+              <div className="totalAndsub mx-8 xsm:mx-4 pt-32">
+                <div className="subtotal py-5 mb-10">
+                  <p className="text-base font-medium capitalize inline-block float-left">
+                    subtotal
+                  </p>
+                  <p className="subtotalPrice inline-block float-right font-semibold">
+                    $122
+                  </p>
                 </div>
+                <div className="subtotal py-5">
+                  <p className="text-base font-medium capitalize inline-block float-left">
+                    total
+                  </p>
+                  <p className="subtotalPrice inline-block float-right font-semibold">
+                    $122
+                  </p>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </main>
@@ -145,18 +151,23 @@ function Checkout() {
 
 export default Checkout;
 
-
-function ProductONCart({name,img,price}){
-
-    return(
-        <div className="productONCart my-8 mx-8 xsm:mx-4 flex justify-between items-center">
-        <div className="nameAndImgProduct flex items-center ">
-        <div className="PcartImg w-24 h-20 rounded inline-block mr-6 relative" notiffi="5">
-             <img src={img} alt="product image" className="w-full h-full object-fill object-center rounded" />
-         </div>
-         <p className="productNameCart inline-block text-base ">{name}</p>
+function ProductONCart({ name, img, price }) {
+  return (
+    <div className="productONCart my-8 mx-8 xsm:mx-4 flex justify-between items-center">
+      <div className="nameAndImgProduct flex items-center ">
+        <div
+          className="PcartImg w-24 h-20 xsm:w-16 xsm:h-16 min-w-16 rounded inline-block mr-6 relative xsm:basis-16"
+          notiffi="5"
+        >
+          <img
+            src={img}
+            alt="product image"
+            className="w-full h-full object-fill object-center rounded"
+          />
         </div>
-        <p className="pricePro text-base text-purple-text">{price}</p>
-     </div>
-    )
+        <p className="productNameCart inline-block text-base ">{name}</p>
+      </div>
+      <p className="pricePro text-base text-purple-text">{price}</p>
+    </div>
+  );
 }
